53c61ee0d8f38a0a1ab73782d58b8ef60bd2256c
Somya Patidar <somyapatidar510@gmail.com>
2026-03-28 10:24:33 +0530

    Server updated

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const express = require('express')
 const path = require('path')
 
 const connectDB = require('./src/config/db')
-
-// ✅ just require redis (no function)
-require('./src/config/redisClient')
+require('./src/config/redisClient') // auto connect
 
 const app = express()
 
 app.use(express.json())
 
+// serve frontend
 app.use(express.static(path.join(__dirname, 'public')))
 
+// connect DB
 connectDB()
 
+// routes
 app.use('/', require('./src/routes/urlRoutes'))
+
+// fallback
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
